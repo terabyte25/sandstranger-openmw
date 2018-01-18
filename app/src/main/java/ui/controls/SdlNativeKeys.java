bf4dbe6f78badaeda679e78fcb5c5bd7a1335f8a
Ilya Zhuravlev <whatever@xyz.is>
2018-01-17 20:59:26 -0500

    Implement virtual gamepad
    # Conflicts:
    #	app/settings-base.cfg
    #	app/src/main/java/ui/controls/SdlNativeKeys.java
    #	app/src/main/java/ui/controls/TouchCameraSimulation.java
    #	app/src/main/res/layout/screencontrols.xml

--- app/src/main/java/ui/controls/SdlNativeKeys.java
+++ app/src/main/java/ui/controls/SdlNativeKeys.java
@@ -16,14 +16,5 @@ public class SdlNativeKeys {
         SDLActivity.onNativeKeyUp(keyCode);
     }
 
-    public static void touchDown(final float x, final float y, final int eventAction, MotionEvent event) {
-        final int touchDevId = event.getDeviceId();
-        int i = event.getActionIndex();
-        final int pointerID = event.getPointerId(i);
-        final float pointerCount = event.getPressure(i);
-        SDLActivity.mSeparateMouseAndTouch = true;
-        SDLActivity.onNativeTouch(touchDevId, 0,MotionEvent.ACTION_MOVE, x, y, 1.0f);
-    }
-
 
 }
